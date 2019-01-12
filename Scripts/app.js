cd7b930d8b58e8268cc2ceea9e7364bb38604c89
sjtype <31806705+sjtype@users.noreply.github.com>
2019-01-12 16:20:32 -0500

    Added Bootstrap support

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,4 @@
+"use strict"; 
 // let myGlobalVariable = 0; use let instead of var (no hoisting), but it's better to encapsulate
 
 // self-executing anonymous function
@@ -5,14 +6,12 @@
 (function(){ // IFFE header
     let myFunctionalVariable = 0;
 
-    /*
     // same as let Start = function() {...} --> pointing to anon function with identifier
     function Start() { // named function
         let myLocalVariable = 0;
         console.log(`App Started...${myFunctionalVariable}`); // use back ticks instead of double quotation marks ("")
         // injects value instead of concatenating
     }
-    */
 
     window.addEventListener("load", Start); // on load, call Start function 
 })();
