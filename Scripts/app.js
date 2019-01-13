cba098e673b6ccce1e81445456b029fa036d11f2
sjtype <31806705+sjtype@users.noreply.github.com>
2019-01-12 22:39:13 -0500

    added notes and corrected Bootstrap link

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,5 @@
-"use strict"; 
-// let myGlobalVariable = 0; use let instead of var (no hoisting), but it's better to encapsulate
+"use strict"; // helps debug some challenges (triggers compiler so that certain things will give errors, ex. assigning values to variables without declaration)
+// let myGlobalVariable = 0; use let instead of var (no hoisting, which can change the written from how it's written), but it's better to encapsulate
 
 // self-executing anonymous function
 // IIFE (Immediately Invoked Function Expression)
@@ -9,9 +9,9 @@
     // same as let Start = function() {...} --> pointing to anon function with identifier
     function Start() { // named function
         let myLocalVariable = 0;
-        console.log(`App Started...${myFunctionalVariable}`); // use back ticks instead of double quotation marks ("")
-        // injects value instead of concatenating
+        console.log(`App Started...${myFunctionalVariable}`); // use back ticks instead of double quotation marks ("") to create template strings when writing to console
+        // injects value instead of concatenating (which would change the variable type) when interpolating
     }
 
-    window.addEventListener("load", Start); // on load, call Start function 
+    window.addEventListener("load", Start); // on load, call Start function (call back)
 })();
